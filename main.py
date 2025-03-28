45a064351d38350e60a6fdf5cc9be36099c91533
Murilo <murilo.fabri123@gmail.com>
2025-03-28 15:49:15 -0300

    adicao do cabecalho

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+'''
+
+Program: main.py
+
+Author: Murilo Fabri Lourenço
+
+Date: 28/03/2025
+
+Description: Esse programa importa a função print_hello do arquivo modulo1.py para imprimir "Hello" na tela.
+
+License: CC BY-SA
+
+'''
+
 from my_test_package.modulo1 import print_hello
 
 print_hello()
